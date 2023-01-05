c0d77992a1e4264f4e66104d4ba8c0b0967cbcd8
Luis <luisjavier_2705@hotmail.com>
2023-01-05 07:25:07 -0500

    cuminacion de imports y exports

---
+++ ts-node/src/data/powers.ts
@@ -0,0 +1,17 @@
+interface Power {
+    id: number;
+    desc: string
+}
+
+const powers: Power[] = [
+    {
+        id: 1,
+        desc: 'money'
+    },
+    {
+        id: 2,
+        desc: 'drug'
+    }
+]
+
+export default powers
--- ts-node/src/index.ts
+++ ts-node/src/index.ts
@@ -1,9 +1,11 @@
+//import { Hero as SuperHero , Hero2 } from "./classes/ero";
+import * as HeroClasses from "./classes/ero"
+import  powers  from "./data/powers";
+const Hero = 123;
+const ironman = new HeroClasses.Hero('ironman', 1, 55)
+console.log(ironman)
 
-
-console.log('Hola Mundo!');
-
-
-
+console.log(powers)
 
 
 
